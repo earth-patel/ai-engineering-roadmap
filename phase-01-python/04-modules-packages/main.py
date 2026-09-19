b1fad2a3a3dbc238eee2b96151300c074829bf13
Earth Patel <earthrpatel@gmail.com>
2026-09-19 17:11:55 +0530

    Add calculator module with basic arithmetic functions

---
+++ phase-01-python/04-modules-packages/main.py
@@ -0,0 +1,7 @@
+import calculator
+# from calculator import add  # Import only specific functions
+# from calculator import subtract, multiply  # Import multiple functions
+# from calculator import *  # Import all functions from the module
+
+result = calculator.add(5, 3)
+print(result)  # Output: 8
