3d1d91c35023e363af7adab71a767ac0fd733fe3
Earth Patel <earthrpatel@gmail.com>
2026-09-19 23:21:54 +0530

    Update test_calculator.py to clarify pytest command and add new logging.py file

--- phase-01-python/12-testing/test_calculator.py
+++ phase-01-python/12-testing/test_calculator.py
@@ -7,6 +7,6 @@ def test_subtract():
   assert subtract(5, 3) == 2
 
 # To run the tests, run the following command in your terminal:
-# pytest
+# pytest or pytest test_calculator.py
 # pytest will find the files that has test_*.py or *_test.py and
 # run the functions that start with test_ in those files.
